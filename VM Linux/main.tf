23c6c54ef745abcaadf9bc2a298d54a70cce7de6
unknown <andra@vibicloud.com>
2021-06-02 14:00:20 +0700

    Update Version

--- VM Linux/main.tf
+++ VM Linux/main.tf
@@ -1,11 +1,11 @@
 provider "azurerm" {
-  version         = "2.0.0"
+  version         = "2.46.0"
   subscription_id = var.subscriptionID
 
   features {}
 }
 
-resource "azurerm_virtual_machine" "APDevVM" {
+resource "azurerm_virtual_machine" "APLinVM" {
   name                  = "APLinux"
   location              = var.location
   resource_group_name   = var.resourceGroupName
